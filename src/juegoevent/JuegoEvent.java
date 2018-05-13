a4b7e702684a5b19b1035e44e2dd4ee9600a379f
karen <karen@LAPTOP-34EDUCNG.home>
2018-05-12 22:58:06 -0300

    subiendo afd

---
+++ src/juegoevent/JuegoEvent.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegoevent;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author karen
+ */
+public class JuegoEvent {
+
+    
+    public static void main(String[] args) {
+      
+  eleccion laeleccion=new eleccion();
+  laeleccion.setTitle("hgdh");
+  laeleccion.setVisible(true);
+ 
+
+ 
+	}
+
+    }
+
+	
+	
+    
+    
+
