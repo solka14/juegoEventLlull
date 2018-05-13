b4e1d83c52ca42fd7cb27c2e9b0c5837300948b2
karen <karen@LAPTOP-34EDUCNG.home>
2018-05-12 23:07:23 -0300

    subiendo afd

---
+++ src/juegoevent/compuAdivina.java
@@ -0,0 +1,210 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegoevent;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author karen
+ */
+public class compuAdivina extends javax.swing.JFrame {
+
+    /**
+     * Creates new form compuAdivina
+     */
+    public compuAdivina() {
+        initComponents();
+        this.setSize(582,350);
+    }
+     int numAleatorio=(int)(1+(Math.random()*100));
+     int minimo=0;
+     int maximo=100;
+     int contador=0;
+public int adivinaVos(){
+   txtNum.setText("El numero que pensaste es "+numAleatorio+"??");
+    
+
+   
+    return 0;
+}
+public void EsMayor (){
+        
+              
+        if (minimo <= numAleatorio && contador<=5){
+            minimo=numAleatorio;}
+        
+               
+        numAleatorio=(int) (Math.random()*(maximo-minimo)+(minimo));
+        this.txtNum.setText("mm si es mayor entonces es"+numAleatorio+"?");
+       contador++;
+        if(contador>5)
+           {
+            this.txtNum.setText("Perdi, ya realice muchos intentos, bien pensado:((");
+            }
+        }
+
+public void EsMenor(){
+    if(maximo>=numAleatorio && contador<=5){
+        maximo=numAleatorio;
+    }
+    numAleatorio=(int)(Math.random()*(maximo-minimo)+(minimo));
+    this.txtNum.setText("amm es menor... entonces es"+numAleatorio+"??");
+    contador++;
+    if(contador>5)
+           {
+            this.txtNum.setText("Perdi, ya realice muchos intentos, bien pensado:((");
+            }
+        
+    
+            
+    
+}
+
+public void EseEs(){
+    
+         this.txtNum.setText("Ganee!! en "+contador+" intentos");
+         
+    
+}
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnMenor = new javax.swing.JButton();
+        btnMayor = new javax.swing.JButton();
+        txtNum = new javax.swing.JLabel();
+        btnEs = new javax.swing.JButton();
+        btnSalir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        btnMenor.setBackground(new java.awt.Color(255, 153, 255));
+        btnMenor.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnMenor.setText("Es mayor");
+        btnMenor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenorActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnMenor);
+        btnMenor.setBounds(405, 160, 100, 23);
+
+        btnMayor.setBackground(new java.awt.Color(255, 153, 255));
+        btnMayor.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnMayor.setText("Es menor");
+        btnMayor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMayorActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnMayor);
+        btnMayor.setBounds(57, 160, 90, 23);
+
+        txtNum.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        txtNum.setForeground(new java.awt.Color(255, 51, 153));
+        txtNum.setText("El numero es:");
+        getContentPane().add(txtNum);
+        txtNum.setBounds(50, 60, 465, 25);
+
+        btnEs.setBackground(new java.awt.Color(255, 153, 255));
+        btnEs.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnEs.setText("Ese es!");
+        btnEs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEsActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnEs);
+        btnEs.setBounds(250, 160, 90, 23);
+
+        btnSalir.setBackground(new java.awt.Color(204, 255, 204));
+        btnSalir.setText("Salir");
+        btnSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalirActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnSalir);
+        btnSalir.setBounds(420, 260, 70, 23);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/juegoevent/fondos/fondo-de-cubos-negros-3D.jpg"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 580, 340);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnMayorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMayorActionPerformed
+this.EsMenor();        // TODO add your handling code here:
+    }//GEN-LAST:event_btnMayorActionPerformed
+
+    private void btnMenorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenorActionPerformed
+       this.EsMayor();
+    }//GEN-LAST:event_btnMenorActionPerformed
+
+    private void btnEsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEsActionPerformed
+this.EseEs();        // TODO add your handling code here:
+    }//GEN-LAST:event_btnEsActionPerformed
+
+    private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
+this.setVisible(false);
+eleccion ele=new eleccion();
+ele.setVisible(true);
+
+    }//GEN-LAST:event_btnSalirActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(compuAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(compuAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(compuAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(compuAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new compuAdivina().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnEs;
+    private javax.swing.JButton btnMayor;
+    private javax.swing.JButton btnMenor;
+    private javax.swing.JButton btnSalir;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel txtNum;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/juegoevent/personaAdivina.java
@@ -0,0 +1,194 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegoevent;
+
+import java.util.Scanner;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author karen
+ */
+public class personaAdivina extends javax.swing.JFrame {
+
+    /**
+     * Creates new form personaAdivina
+     */
+    public personaAdivina() {
+        initComponents();
+      this.setSize(597,345);
+    }
+   int numero=1+(int)(Math.random()*100);
+   int contador=0;
+  
+public int adivinoyo(int valor){
+ 
+  boolean ganar=false;
+  System.out.println("Tenes que adivinar el numero que pensé en un rango del 1 al 100");
+   
+     contador++;
+     
+      if(valor==numero){
+          txtNumero.setText("Felicidades crack!! Ganaste en "+contador+" intentos");
+     
+      this.setVisible(true);
+      
+      ganar=true;
+      }else{
+         String resul="";
+         if(numero>valor){
+          resul="mayor";
+          txtNumero.setText("mi numero no es "+valor+" mi numero es mayor");
+         }else{
+          resul="menor";
+          txtNumero.setText("mi numero no es"+valor+" mi numero es menorrr");
+         }
+        
+      }
+   
+if(contador==20 && ganar==false) {
+   //'contador==20' representa el numero de oportunidades del jugador
+  txtNumero.setText("Ups, no has adivinado, el numero era el "+numero+ " ,suerte para la proxima!!");
+    
+    btnAceptar.setEnabled(false);
+   }
+        return 0;
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        btnAceptar = new javax.swing.JButton();
+        btnNumero = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        txtNumero = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(800, 600));
+        getContentPane().setLayout(null);
+
+        jLabel1.setBackground(new java.awt.Color(255, 102, 102));
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 102, 255));
+        jLabel1.setText("Elegi un numero:");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(31, 37, 182, 22);
+
+        btnAceptar.setText("Aceptar");
+        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAceptarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnAceptar);
+        btnAceptar.setBounds(275, 37, 90, 23);
+
+        btnNumero.setText("0");
+        btnNumero.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNumeroActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnNumero);
+        btnNumero.setBounds(217, 38, 48, 20);
+
+        jButton1.setBackground(new java.awt.Color(255, 0, 255));
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jButton1.setText("Salir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(263, 230, 100, 23);
+
+        txtNumero.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        txtNumero.setForeground(new java.awt.Color(102, 255, 153));
+        txtNumero.setText("Holi!");
+        getContentPane().add(txtNumero);
+        txtNumero.setBounds(31, 103, 324, 35);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/juegoevent/fondos/fondo-de-cubos-negros-3D.jpg"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(0, 0, 600, 370);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
+       //en estas lineas lo que hago es convertir el numero en un entero(porque sino lo tomas como String), y en caso
+       //que se escriba una letra o palabra o negativos, saldra la excepcion
+        String texto = btnNumero.getText(); 
+       int numIngre;
+      try{
+       numIngre =  Integer.parseInt(texto);
+       this.adivinoyo(numIngre);
+      }
+       catch(Exception ex){
+           System.out.println("que haces?????, solo numeros y posotivos :D !");
+    }
+    
+       
+    }//GEN-LAST:event_btnAceptarActionPerformed
+
+    private void btnNumeroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNumeroActionPerformed
+               
+    }//GEN-LAST:event_btnNumeroActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+this.setVisible(false);  
+eleccion ele=new eleccion();
+ele.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(personaAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(personaAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(personaAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(personaAdivina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new personaAdivina().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAceptar;
+    private javax.swing.JTextField btnNumero;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel txtNumero;
+    // End of variables declaration//GEN-END:variables
+}
